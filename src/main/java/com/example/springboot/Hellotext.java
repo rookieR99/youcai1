5f0d7df85451ca1f77401b4d12fbc7b9ffff4fae
rookieR99 <stth8067@163.com>
2022-11-24 15:04:41 +0800

    first

---
+++ src/main/java/com/example/springboot/Hellotext.java
@@ -0,0 +1,12 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Hellotext {
+@RequestMapping("/hello")
+    public String hello() {
+
+        return "Hello";
+    }
+}
